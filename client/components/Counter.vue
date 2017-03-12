50a415fc43ddceaa7f2a892b5641065ee585d1e1
Ben <ben.k.richter@gmail.com>
2017-03-12 16:02:11 +0100

    Add explanation text and format

--- client/components/Counter.vue
+++ client/components/Counter.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="counter-wrapper">
-    <div class="counter">
-      {{ count }}
+    <div class="counter-wrapper">
+        <div class="counter">
+            {{ count }}
+        </div>
+        <button @click="$store.commit('INCREMENT')">Increment</button>
+        <button @click="$store.commit('DECREMENT')">Decrement</button>
+        <button @click="$store.dispatch('incrementAsync')">Increment Async</button>
     </div>
-    <button @click="$store.commit('INCREMENT')">Increment</button>
-    <button @click="$store.commit('DECREMENT')">Decrement</button>
-    <button @click="$store.dispatch('incrementAsync')">Increment Async</button>
-  </div>
 </template>
 
 <script>
--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="page">
-    <counter></counter>
-    <p>
-      To get started, edit files in <code>./client</code> and save.
-    </p>
-  </div>
+    <div class="page">
+        <h1>Steam Key Tumbler</h1>
+
+        <p>Put in Steam Keys and share the Key with your Community</p>
+
+        <textarea name="steamkeys" id="key-input" cols="60" rows="8"></textarea>
+
+        <counter></counter>
+
+        <p>Share Link: <code>/share?id=123</code></p>
+    </div>
 </template>
 
 <script>
