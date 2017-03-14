21b361f76929191146a3e0cd46f1fb3c48fa4886
Ben <ben.k.richter@gmail.com>
2017-03-14 15:26:52 +0100

    Fix intendation and add missing semicolon

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -8,28 +8,28 @@ const FriendlyErrors = require('friendly-errors-webpack-plugin');
 
 base.devtool = 'eval-source-map';
 base.plugins.push(
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('development')
-  }),
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoEmitOnErrorsPlugin(),
-  new FriendlyErrors()
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new FriendlyErrors()
 );
 
 // push loader for css files
 _.cssProcessors.forEach(processor => {
-  let loaders;
-  if (processor.loader === '') {
-    loaders = ['postcss-loader'];
-  } else {
-    loaders = ['postcss-loader', processor.loader];
-  }
-  base.module.loaders.push(
-    {
-      test: processor.test,
-      loaders: ['style-loader', _.cssLoader].concat(loaders)
+    let loaders;
+    if (processor.loader === '') {
+        loaders = ['postcss-loader'];
+    } else {
+        loaders = ['postcss-loader', processor.loader];
     }
-  );
+    base.module.loaders.push(
+        {
+            test: processor.test,
+            loaders: ['style-loader', _.cssLoader].concat(loaders)
+        }
+    );
 });
 
 module.exports = base;
--- client/app.js
+++ client/app.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import { sync } from 'vuex-router-sync'
-import App from './components/App'
-import router from './router'
-import store from './store'
-sync(store, router)
+import Vue from 'vue';
+import { sync } from 'vuex-router-sync';
+import App from './components/App';
+import router from './router';
+import store from './store';
+sync(store, router);
 
 const app = new Vue({
-  router,
-  store,
-  ...App
-})
+    router,
+    store,
+    ...App
+});
 
-export { app, router, store }
+export { app, router, store };
