df7729bc1ff0f43fd3b473900d54eed3b81a21f1
Ben <ben.k.richter@gmail.com>
2017-03-16 16:05:48 +0100

    Data binding

--- client/components/KeyInput.vue
+++ client/components/KeyInput.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="key-input">
-        <p>Put in Steam Keys and share the Key with your Community</p>
-        <textarea name="steamkeys" id="key-input" cols="60" rows="8"></textarea>
+      <div class="key-input">
+        <p>Put in Steam Keys and share the Key with your Community {{message}}</p>
+         <textarea name="steamkeys" cols="60" rows="8" v-model="message"></textarea>
     </div>
 </template>
 
 <script>
-
+    export default {
+        data() {
+            return {
+                message: 'Add keys here'
+            }
+        }
+    }
 </script>
 
 <style scoped>
